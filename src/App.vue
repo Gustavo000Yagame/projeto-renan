86d46e853d3c384c6e1be7c6358e2fb98387ebac
GUSTAVO CARDOSO GUARESI <gustavo_guaresi@estudante.sesisenai.org.br>
2026-04-13 22:23:40 -0300

    commit

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,7 @@ import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 
 import type Funcionario from './models/Funcionario';
 
+const ordem = ref('')
 const funcionario = ref({} as Funcionario)
 const lista = ref([] as Funcionario[])
 
@@ -18,24 +19,61 @@ function excluirFuncionario(id: number) {
 }
 
 function editarFuncionario(id: number) {
+  const item = lista.value.find(item => item.id === id)
+  if (item) {
+    funcionario.value = { ...item }
+    excluirFuncionario(id)
+  }
 
 }
+  function ordenarCom() {
+    if (ordem.value === 'salario')
+      lista.value.sort((a, b) => a.salario - b.salario)
+    else if (ordem.value === 'nome')
+    lista.value.sort((a, b) => a.nome.localeCompare(b.nome))
+    else if (ordem.value === 'cargo')
+    lista.value.sort((a, b) => a.cargo.localeCompare(b.cargo))
+  }
+
+
 
 
 </script>
 
 <template>
+  <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">SENAI</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Sobre</a>
+        </li>
+        </ul>
+    </div>
+  </div>
+</nav>
+
+
+
+
   <div class="container">
     <div class="row">
       <div class="col">
         <form @submit.prevent="salvaFuncionario">
           <div class="mb-3">
             <label for="nome" class="form-label">Nome</label>
-            <input v-model="funcionario.nome" type="text" class="form-control" id="nome">
+            <input v-model="funcionario.nome" type="text" class="form-control" id="nome" required>
           </div>
           <div class="mb-3">
             <label for="preco" class="form-label">Email</label>
-            <input v-model="funcionario.email" type="email" class="form-control" id="preco">
+            <input v-model="funcionario.email" type="email" class="form-control" id="preco" required>
           </div>
           <div class="mb-3">
             <label for="desc" class="form-label">Cargo</label>
@@ -47,14 +85,21 @@ function editarFuncionario(id: number) {
           </div>
           <button type="submit" class="btn btn-primary">Salvar</button>
         </form>
+
+        <h4>Ordenação</h4>
+        <select v-model="ordem" class="form-select" @change="ordenarCom" >
+          <option value="nome">Nome</option>
+          <option value="cargo">Cargo</option>
+          <option value="salario">Salário</option>
+        </select>
+
         <table class="table mt-3">
           <thead>
             <tr>
-              <th scope="col">#</th>
               <th scope="col">Nome</th>   
               <th scope="col">Email</th>
               <th scope="col">Cargo</th>
-              <th scope="col">Salario</th>
+              <th scope="col">Salário</th>
               <th scope="col">Ações</th>
 
             </tr>
@@ -69,7 +114,7 @@ function editarFuncionario(id: number) {
 
 
               <button type="button" class="btn btn-danger" @click="excluirFuncionario(item.id)">🗑️</button>
-              <button type="button" class="btn btn-warning">📝</button>
+              <button type="button" class="btn btn-warning" @click="editarFuncionario(item.id)">📝</button>
             </tr>
           </tbody>
         </table>
@@ -80,9 +125,23 @@ function editarFuncionario(id: number) {
 
 <style>
 
+
+.navbar {
+  margin-left: 137px;
+  display: flex;
+  background-color: rgb(167, 167, 167);
+
+}
+
 body{
   background-color: #f8f9fa;
-  margin-top: 50px;
 }
+
+
+.h2 {
+  margin-top: 30px;
+}
+
+
 
 </style>
